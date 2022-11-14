11c37c2aab9d13d416d414743f7064e8d0803edd
heshuai <heshuai.sec@gmail.com>
2022-11-14 16:32:55 +0800

    log import error

--- pyEngine/inference_engine/universal_ie.py
+++ pyEngine/inference_engine/universal_ie.py
@@ -9,28 +9,36 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-VALID_IE_DICT ={}
+VALID_IE_DICT = {}
 try:
     from .mnn_ie import InferenceWithMNN
+
     VALID_IE_DICT['mnn'] = InferenceWithMNN
-except:
+except Exception as e:
     logger.warning('mnn not valid')
+    logger.error(e)
 try:
     from .onnx_ie import InferenceWithOnnx
+
     VALID_IE_DICT['onnx'] = InferenceWithOnnx
-except:
+except Exception as e:
     logger.warning('onnx not valid')
+    logger.error(e)
 try:
     from .tflite_ie import InferenceWithTFLite
+
     VALID_IE_DICT['tflite'] = InferenceWithTFLite
-except:
+except Exception as e:
     logger.warning('tflite not valid')
+    logger.error(e)
 
 try:
     from .tensorflow_ie import InferenceWithPb
+
     VALID_IE_DICT['pb'] = InferenceWithPb
-except:
+except Exception as e:
     logger.warning('tensorflow not valid')
+    logger.error(e)
 
 __all__ = ['IE']
 
@@ -39,7 +47,7 @@ class IE:
     def __init__(self, model_dir, *args, **kwargs):
         model_format = model_dir.split('.')[-1]
         if model_format not in VALID_IE_DICT.keys():
-            logger.error('package for {} not installed!'.format(model_format))
+            logger.error('backend fro {} is invalid!'.format(model_format))
             exit(0)
         else:
             self.ie = VALID_IE_DICT[model_format](model_dir, *args, **kwargs)
@@ -48,4 +56,3 @@ class IE:
                 *args, **kwargs):
         result = self.ie.predict(input_data, *args, **kwargs)
         return result
-
